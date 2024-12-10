dfb30b402fb8311d0a9a991b5c9fb8561b160d5e
1F10220149 <s1F102201492@iniad.org>
2024-12-10 13:45:13 +0900

    initial commit

---
+++ app/calender/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Calender from '../components/Calender'
+
+const page = () => {
+  return (
+    <div>
+        <Calender />
+    </div>
+  )
+}
+
+export default page
