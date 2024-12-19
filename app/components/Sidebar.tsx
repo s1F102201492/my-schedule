c01218f0f252ccbdb4c3ecb4b1aa2a974f747875
1F10220149 <s1F102201492@iniad.org>
2024-12-19 18:07:56 +0900

    サイドバーとヘッダーテンプレ完成

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,43 +1,38 @@
 'use client'
 
-import React, { useState } from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-export default function Sidebar() {
-  const [open, setOpen] = useState(false);
+interface SidebarProps {
+  open: boolean;
+  toggleDrawer: (open: boolean) => () => void;
+}
 
-  const toggleDrawer = (newOpen: boolean) => () => {
-    setOpen(newOpen);
-  };
-
-  const DrawerList = (
-    <Box sx={{ width:250  }} role="presentation" onClick={toggleDrawer(false)}>
-        <MenuIcon />
-      <List>
-        {['Menu1', 'Menu2', 'Menu3', 'Menu4'].map((text) => (
-          <ListItem key={text}>
-            <ListItemButton>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
+export default function Sidebar({ open, toggleDrawer }: SidebarProps) {
   return (
-    <div>
-      <Button onClick={toggleDrawer(true)}><MenuIcon /></Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {DrawerList}
-      </Drawer>
-    </div>
+    <Drawer open={open} onClose={() => {toggleDrawer(false)}}>
+      <Box sx={{ width: 250 }} role="presentation">
+        {/* ハンバーガーアイコンをサイドバーの内部にも表示して、クリックでドロワーを閉じる */}
+        <IconButton onClick={() => {toggleDrawer(false)}} sx={{ mt: 1, ml: 1 }}>
+          <MenuIcon />
+        </IconButton>
+        <List onClick={() => {toggleDrawer(false)}}>
+          {['Menu1', 'Menu2', 'Menu3', 'Menu4'].map((text) => (
+            <ListItem key={text}>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
   );
 }
