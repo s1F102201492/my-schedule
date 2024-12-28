b821ecb0e46dcbf23b03caae012505777a3290d6
1F10220149 <s1F102201492@iniad.org>
2024-12-28 14:32:27 +0900

    動的表示

---
+++ app/listcomponents/TodoItem.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { Button, CardActions, CardHeader, Typography } from '@mui/material';
+
+interface TodoProps {
+    id: string;
+    title: string;
+    description: string | null;
+    continuedays: number;
+    checked: Boolean;
+    priority: string;
+    startdate: string;
+    enddate: string;
+    interval: number | null;
+  };
+  
+interface TodoItemProps {
+    todos: TodoProps;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({ todos }) => {
+  return (
+    <div>
+        <Card sx={{ mt: 2, mx: 2, boxShadow: 3 }}>
+            <CardHeader title={todos.title} />
+            <CardContent>
+                <Typography>
+                    {todos.description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button variant='contained'>完了</Button>
+                <Button variant='outlined'>編集</Button>
+                <Button variant='outlined'>削除</Button>
+            </CardActions>
+        </Card>
+    </div>
+  )
+}
+
+export default TodoItem
