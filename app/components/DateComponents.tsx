ccf9a92e0d7775b4ac34459d8dcc132400547041
1F10220149 <s1F102201492@iniad.org>
2024-12-30 17:39:18 +0900

    入力フォーム作成途中までできた

--- app/components/DateComponents.tsx
+++ app/components/DateComponents.tsx
@@ -1,4 +1,5 @@
 import { JSX } from '@emotion/react/jsx-runtime';
+import { Margin } from '@mui/icons-material';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
@@ -8,15 +9,17 @@ dayjs.locale('ja'); // カレンダーの曜日のフォーマット
 
 export const DateComponents = (): JSX.Element => {
   return (
-    <div>
+    <div style={{marginTop: 10}}>
         <LocalizationProvider
         dateAdapter={AdapterDayjs}
         dateFormats={{ year: 'YYYY年' }} // カレンダー内の年一覧のフォーマット
         >
         <DatePicker
-            label="YYYY/MM/DD"
+            label="日付"
             format="YYYY/MM/DD" // テキストエリア内のフォーマット
-            slotProps={{ calendarHeader: { format: 'YYYY年MM月' } }} // カレンダーヘッダーのフォーマット
+            slotProps={{ textField: { required: true } ,
+            calendarHeader: { format: 'YYYY年MM月' }}} // カレンダーヘッダーのフォーマット
+            
         />
         </LocalizationProvider>
     </div>
