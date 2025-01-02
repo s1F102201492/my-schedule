ba6ebf1a5f4fb00a065111cfab0a683bbd8498fb
1F10220149 <s1F102201492@iniad.org>
2025-01-02 21:30:59 +0900

    ナビゲーションバー途中

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,31 +6,62 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { Stack } from '@mui/material';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import HomeIcon from '@mui/icons-material/Home';
+import EventIcon from '@mui/icons-material/Event';
+import MenuIcon from '@mui/icons-material/Menu';
+import Link from 'next/link';
 
 const Header = () => {
+  const [value, setValue] = React.useState('');
+  
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
   return (
     <Box>
       <AppBar position="static">
         <Toolbar>
-        <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ marginRight: 6 }}>
             My schedule
-        </Typography>
-          <Stack direction="row" spacing={6} sx={{ flexGrow: 1, textAlign: 'center' }}>
-            <Box>
-              <Button variant="contained">Text</Button>
-            </Box>
-            <Box>
-              <Typography>Right</Typography>
-            </Box>
-            <Box>
-              <Typography>Right</Typography>
-            </Box>
-            <Box>
-              <Typography>Right</Typography>
-            </Box>
-          </Stack>
-
+          </Typography>
+          {/* ここから */}
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={handleChange}
+            sx={{ flexGrow: 1, backgroundColor: 'inherit' }}
+          >
+            <BottomNavigationAction 
+              label="Home" 
+              value='Home'
+              icon={<HomeIcon />} 
+              component={Link} 
+              href="/"
+            />
+            <BottomNavigationAction 
+              label="カレンダー" 
+              value='calender'
+              icon={<EventIcon />} 
+              component={Link} 
+              href="/calender"
+            />
+            <BottomNavigationAction 
+              label="Menu3" 
+              value='Menu3'
+              icon={<MenuIcon />}
+              href="/" 
+            />
+            <BottomNavigationAction 
+              label="Menu4" 
+              value='Menu4'
+              icon={<MenuIcon />} 
+              href="/calender"
+            />
+          </BottomNavigation>
+          {/* ここまでをBottom navigationに変更 */}
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
