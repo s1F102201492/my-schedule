e042df99cf5ef45aa88e38c9e1372feea84e91bd
1F10220149 <s1F102201492@iniad.org>
2025-01-05 20:24:48 +0900

    fullcalendarを追加

--- app/components/Calender.tsx
+++ app/components/Calender.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import allLocales from '@fullcalendar/core/locales-all';
 
 const Calender = () => {
   return (
-    <div>Calender</div>
+    <div>
+      <FullCalendar plugins={[dayGridPlugin]}
+      locales={allLocales} locale='ja' />
+    </div>
   )
 }
 
