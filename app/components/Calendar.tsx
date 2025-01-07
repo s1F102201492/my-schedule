5eec7aff0385cff05edd9b12e754aa3fd8e4f365
1F10220149 <s1F102201492@iniad.org>
2025-01-07 15:04:35 +0900

    カレンダーアラート表示

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -3,18 +3,20 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import allLocales from '@fullcalendar/core/locales-all';
-import { DateSelectArg } from '@fullcalendar/core/index.js';
+import { DateSelectArg, EventApi, EventClickArg } from '@fullcalendar/core';
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from '@fullcalendar/list';
 
 const Calendar = () => {
   const handleDateSelect = (selectionInfo: DateSelectArg) => {
     console.log('selectionInfo: ', selectionInfo); // 選択した範囲の情報をconsoleに出力
-    const calendarApi = selectionInfo.view.calendar;
-  
-    calendarApi.unselect(); // 選択した部分の選択を解除
   };
 
+  const handleEventClick = (clickInfo: EventClickArg) => {
+    const event: EventApi = clickInfo.event;
+    alert('イベントタイトル: ' + event.title + '\n詳細: ' + (event.extendedProps.description || '説明はありません'));
+  };
+  
   return (
     <div>
       <FullCalendar plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
@@ -22,6 +24,7 @@ const Calendar = () => {
       events={'https://fullcalendar.io/api/demo-feeds/events.json'}
       select={handleDateSelect}
       selectable={true}
+      eventClick={handleEventClick}
       headerToolbar={{
         left: 'prev,next today',
         center: 'title',
