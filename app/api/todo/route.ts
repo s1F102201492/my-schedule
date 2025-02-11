e0e102fe45d56492feddc377b81eb22c93d76c4c
1F10220149 <s1F102201492@iniad.org>
2025-02-11 22:48:57 +0900

    POST成功

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -28,13 +28,16 @@ export const GET = async (req: Request, res: NextResponse) => {
 // タスク追加用API
 export const POST = async (req: Request, res: NextResponse) => {
     try {
-        const { id, title, description, startdate, enddate, checkedDates, continuedays, interval, color } = await req.json();
-
+        const jsondata = await req.json();
+        const { id, title, description, continuedays, checkedDates, startdate, enddate, interval, color } = jsondata
+        const formattedStartDate = new Date(startdate.replace(/\//g, '-'));
+        const formattedEndDate = new Date(enddate.replace(/\//g, '-'));
         await main();
-        const posttodo = await prisma.todos.create({data: { id, title, description, startdate, enddate, checkedDates, continuedays, interval, color }})
+
+        const posttodo = await prisma.todos.create({data: { id, title, description, continuedays, checkedDates, startdate:formattedStartDate, enddate:formattedEndDate, interval, color }})
         return NextResponse.json({message:"success", posttodo }, {status: 201});
     } catch (err) {
-        return NextResponse.json({message:"Error", err }, {status: 500})
+        return NextResponse.json({message:"Error", error:err }, {status: 500})
     } finally {
         await prisma.$disconnect();
     }
