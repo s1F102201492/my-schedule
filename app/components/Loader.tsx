796d13209f4ab70c8d42610b372b4bb330b62dc1
1F10220149 <s1F102201492@iniad.org>
2025-02-15 11:00:17 +0900

    loader修正

---
+++ app/components/Loader.tsx
@@ -0,0 +1,37 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+import FadeLoader from 'react-spinners/FadeLoader'
+
+interface loadingType {
+    loading: boolean;
+};
+
+const Loader: React.FC<loadingType> = ({ loading }) => {
+  return (
+    <div>
+        <Box
+        sx={{
+            display: "flex",
+            flexDirection: "column", // 縦方向に配置
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh", // 画面全体を占める
+        }}
+        >
+            <Typography
+                sx={{
+                mb: 2, // 下に余白を追加
+                fontSize: "1.5rem",
+                fontWeight: "bold",
+                color: "#1769aa",
+                }}
+            >
+                処理中...
+            </Typography>
+        <FadeLoader color="#2196f3" loading={loading} />
+        </Box>
+    </div>
+  )
+}
+
+export default Loader
--- app/components/TodoContext.tsx
+++ app/components/TodoContext.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { Box, Grid2 } from '@mui/material';
 import React, { createContext, useState, ReactNode, useEffect } from 'react';
-import FadeLoader from "react-spinners/FadeLoader";
+import Loader from './Loader';
 
 interface TodoProps {
   id: number;
@@ -27,7 +26,7 @@ export const TodoContext = createContext<TodoContextType | undefined>(undefined)
 
 export const TodoProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [todos, setTodos] = useState<TodoProps[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const fetchAllTodos = async () => {
     try {
@@ -61,18 +60,7 @@ export const TodoProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
   
   return (
     <TodoContext.Provider value={{ todos, setTodos, toggleChecked, fetchAllTodos }}>
-      {loading ? (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <FadeLoader color="#2196f3" />
-    </Box>
-  ) : children}
+      {loading ? <Loader loading={loading}/> : children}
     </TodoContext.Provider>
   );
   };
