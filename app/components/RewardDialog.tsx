409c098fafd75eed06e2d92d040db809cc640a69
1F10220149 <s1F102201492@iniad.org>
2025-03-20 10:35:46 +0900

    今日のタスク終了アニメーション追加

--- app/components/RewardDialog.tsx
+++ app/components/RewardDialog.tsx
@@ -1,5 +1,6 @@
 import medalAnimation from "@/public/animations/Animation - 1742351524507.json";
-import { Dialog } from "@mui/material";
+import Lottie from "lottie-react";
+import { Box, Dialog, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 import React from 'react'
 
 interface rewardProps {
@@ -10,8 +11,24 @@ interface rewardProps {
 const RewardDialog:React.FC<rewardProps> = ({open, setOpen}) => {
   return (
     <div>
-        <Dialog open={open} onClose={() => setOpen(false)}>
-
+        <Dialog open={open} onClose={() => setOpen(false)} >
+          <DialogContent>
+            <Box sx={{
+              display: "flex",
+              justifyContent: "center", // 水平方向中央
+              height: "200px", // 画面全体の高さ
+              width: "520px", // 画面全体の幅
+            }}>
+              <Lottie animationData={medalAnimation} loop={false} />
+            </Box>
+            <DialogTitle sx={{display: "flex",
+              justifyContent: "center"}}>おめでとうございます！</DialogTitle>
+            <DialogContentText sx={{display: "flex",
+              justifyContent: "center"}}>
+              今日のタスクが全て終了しました！
+            </DialogContentText>
+          </DialogContent>
+          
         </Dialog>
     </div>
   )
