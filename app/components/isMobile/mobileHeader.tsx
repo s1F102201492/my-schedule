56811b0f4ccb7d249364b5c064bf6a03a42210f3
1F10220149 <s1F102201492@iniad.org>
2025-09-22 21:45:29 +0900

    モバイルヘッダーほぼ完成（あとは使い方ガイドのスライドのリンクを埋め込むだけ）

--- app/components/isMobile/mobileHeader.tsx
+++ app/components/isMobile/mobileHeader.tsx
@@ -5,21 +5,14 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Avatar from "@mui/material/Avatar";
 import MenuIcon from '@mui/icons-material/Menu';
-import HomeIcon from "@mui/icons-material/Home";
-import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
-import EventIcon from "@mui/icons-material/Event";
-import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import EqualizerOutlinedIcon from "@mui/icons-material/EqualizerOutlined";
-import { Button, IconButton, Menu } from "@mui/material";
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import { Button, Icon, IconButton, Menu, Tooltip } from "@mui/material";
 import Sidebar from "../Sidebar";
+import ArticleIcon from '@mui/icons-material/Article';
 import { AuthContext } from "../../context/AuthContext";
 
 const MobileHeader = () => {
-    const pathname = usePathname();
 
     const [hambergerMenu, setHambergerMenu] = useState<boolean>(false);
     const handleHambergerMenu = () => {
@@ -52,21 +45,45 @@ const MobileHeader = () => {
                                 whiteSpace: "nowrap",
                                 overflow: "visible",
                                 textOverflow: "clip",
-                                alignItems: "center" }}
+                                alignItems: "center",
+                                color: "white" }}
                             noWrap>
                             Best practice
                         </Typography>
 
                         <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
-                            <IconButton
-                                size="large"
-                                edge="start"
-                                color="inherit"
-                                aria-label="menu"
-                                onClick={handleHambergerMenu}
-                            >
-                                <MenuIcon color="inherit"/>
-                            </IconButton>
+                            <Tooltip title="お問い合わせフォーム">
+                                <IconButton
+                                    size="large"
+                                    edge="start"
+                                    aria-label="inquiry"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    href="https://docs.google.com/forms/d/e/1FAIpQLSfZqnEIjM4MZkNY5Nm8UszqVFhNqJrUmWBANIlu3WxAvRma6g/viewform?usp=dialog"
+                                >
+                                    <ArticleIcon />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="使い方ガイド">
+                                <IconButton
+                                    size="large"
+                                    edge="start"
+                                    sx={{ ml: 1 }}
+                                    aria-label="help">
+                                    <HelpOutlineIcon />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="メニューを開く">
+                                <IconButton
+                                    size="large"
+                                    edge="start"
+                                    sx={{ ml: 1 }}
+                                    aria-label="menu"
+                                    onClick={handleHambergerMenu}
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                            </Tooltip>
                         </Box>
                     </Box>
                 </Toolbar>
