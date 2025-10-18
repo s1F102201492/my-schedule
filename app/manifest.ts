ed4c9bf384ecaf728a5027a89e5b4b2fbbef9b33
1F10220149 <s1F102201492@iniad.org>
2025-10-18 11:08:43 +0900

    pwaの実装完了

---
+++ app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Best Practice',
+    short_name: 'Best Practice',
+    description: 'Best Practice',
+    start_url: '/userauth',
+    display: 'minimal-ui',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/img/BestPracticeIcon.png',
+        sizes: '512x512',  // 実際のサイズを指定
+        type: 'image/png',
+      },
+      {
+        src: '/img/BestPracticeIcon.png',
+        sizes: '192x192',  // 小さいサイズも用意すると良い
+        type: 'image/png',
+      },
+    ],
+  }
+}
+
